f5e4fe1509a2cb7d29bb64aa630aebacfcaa010b
simphiwetebe <simphiwetebe@gmail.com>
2024-03-21 22:07:53 +0200

    Added route for getting rocket by id

--- app.js
+++ app.js
@@ -4,21 +4,44 @@ const client = require('./config/redisClient')
 const responseTime = require('response-time')
 
 const PORT = process.env.PORT || 4000
+const API_URL = 'https://api.spacexdata.com/v3/rockets'
 
 const app = express()
 app.use(responseTime())
 
-app.get('/rockets', async (req, res, next)=> {
+app.get('/rockets', async (req, res)=> {
   try {
     const cachedData = await client.get('rockets')
 
-    if(cachedData) res.send(cachedData)
+    if(cachedData) {
+      res.send(cachedData)
+      return
+    }
 
-    const { data } = await axios.get('https://api.spacexdata.com/v3/rockets')
+    const { data } = await axios.get(API_URL)
     client.set('rockets', JSON.stringify(data))
     res.send(data)
   } catch (error) {
     res.send(error.message)
+  }
+})
+
+app.get('/rockets/:rocket_id', async (req, res)=> {
+  const { rocket_id } = req.params
+
+  try {
+    const cachedData = await client.get(rocket_id)
+
+    if(cachedData) {
+      res.send(cachedData)
+      return
+    }
+
+    const { data } = await axios.get(`${API_URL}/${rocket_id}`)
+    client.set(rocket_id, JSON.stringify(data))
+    res.send(data)
+  } catch (error) {
+    console.log(error.message)
   }
 })
 
